e13da452326a081820ab6b707b2b20d3e37bc4f5
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-10-24 00:29:24 +0700

    update notification

--- src/components/NotificationPopup.tsx
+++ src/components/NotificationPopup.tsx
@@ -1,43 +1,71 @@
 import classNames from "classnames"
+import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import { SERVER_ADDRESS } from "../constants/SystemConstant";
+import { FaEllipsisH } from "react-icons/fa";
+
+
 
 interface NotificationPopupProps {
   show?: boolean
 }
 
+
+
+
+
 export default function NotificationPopup(props: NotificationPopupProps) {
+  const [notificationData, setNotificationData] = useState([])
+
+  useEffect(() => {
+    axios.post(`${SERVER_ADDRESS}api/notifications/user`, {
+      id: 12
+    })
+    .then(res => {
+      // console.log(res.data.data);
+      setNotificationData(res.data.data)
+    })
+  }, [])
+
+  const handleIsRead = () => {
+    
+    axios.put(`${SERVER_ADDRESS}api/notifications/changeStatus/all`, {
+      userId: 12
+    })
+  }
+
+  const notificationItems = (item: any) => {
+    return (
+
+      <div className='card bg-transparent-card w-100 mb-0 border-0 ps-0 ' style={{background: item.status === '0' ? '#f5f5f5' : '#fff', padding: 10, flexDirection: "row", width: 1000}}>
+        <img src='/assets/images/user-8.png' alt='user' className='w70' style={{paddingLeft: 10}}/>
+        <div style={{paddingLeft: 10, width: 250}}>
+        <h5 className='font-xsss text-grey-900 fw-700 d-block mb-1 mt-1' style={{width: 230}}>
+          Hanna 
+        <span className='text-grey-400 font-xsssss fw-600 float-right mt-1'> 3 min</span>
+        </h5>
+        <h6 className='text-grey-500 fw-500 font-xssss lh-4' style={{maxLines: 2}}>{(item.content).length > 50 ? `${item.content.substring(0, 50)}...` : item.content}</h6>
+        </div>
+        {/* <div style={{ position: 'absolute', width: 40, height: 35, borderRadius: 50, background: 'red' }}>
+
+        <FaEllipsisH  />
+        </div> */}
+      </div>
+    )
+  }
+
   return (
     <div
       className={classNames('dropdown-menu notification-dropdown rounded-xxl right-0 border-0 p-4 shadow-lg', props.show ? 'show' : '')}
       aria-labelledby='dropdownMenu3'>
-      <h4 className='fw-700 font-xss mb-4'>Notification</h4>
-      <div className='card bg-transparent-card w-100 mb-3 border-0 ps-5'>
-        <img src='/assets/images/user-8.png' alt='user' className='w40 position-absolute left-0' />
-        <h5 className='font-xsss text-grey-900 fw-700 d-block mb-1 mt-0'>
-          Hendrix Stamp <span className='text-grey-400 font-xsssss fw-600 float-right mt-1'> 3 min</span>
-        </h5>
-        <h6 className='text-grey-500 fw-500 font-xssss lh-4'>There are many variations of pass..</h6>
+      <h4 className='fw-700 font-xss mb-2'>Notification</h4>
+      <div className="mb-3">
+      <button onClick={handleIsRead} style={{background: '#1e74fd', color: '#fff', borderRadius: 50, paddingTop: 3, paddingBottom: 3, paddingLeft: 10, paddingRight:10, fontSize: 13, fontWeight: 'bold'}}>Đánh dấu tất cả đã đọc</button>
       </div>
-      <div className='card bg-transparent-card w-100 mb-3 border-0 ps-5'>
-        <img src='/assets/images/user-4.png' alt='user' className='w40 position-absolute left-0' />
-        <h5 className='font-xsss text-grey-900 fw-700 d-block mb-1 mt-0'>
-          Goria Coast <span className='text-grey-400 font-xsssss fw-600 float-right mt-1'> 2 min</span>
-        </h5>
-        <h6 className='text-grey-500 fw-500 font-xssss lh-4'>Mobile Apps UI Designer is require..</h6>
-      </div>
-      <div className='card bg-transparent-card w-100 mb-3 border-0 ps-5'>
-        <img src='/assets/images/user-7.png' alt='user' className='w40 position-absolute left-0' />
-        <h5 className='font-xsss text-grey-900 fw-700 d-block mb-1 mt-0'>
-          Surfiya Zakir <span className='text-grey-400 font-xsssss fw-600 float-right mt-1'> 1 min</span>
-        </h5>
-        <h6 className='text-grey-500 fw-500 font-xssss lh-4'>Mobile Apps UI Designer is require..</h6>
-      </div>
-      <div className='card bg-transparent-card w-100 border-0 ps-5'>
-        <img src='/assets/images/user-6.png' alt='user' className='w40 position-absolute left-0' />
-        <h5 className='font-xsss text-grey-900 fw-700 d-block mb-1 mt-0'>
-          Victor Exrixon <span className='text-grey-400 font-xsssss fw-600 float-right mt-1'> 30 sec</span>
-        </h5>
-        <h6 className='text-grey-500 fw-500 font-xssss lh-4'>Mobile Apps UI Designer is require..</h6>
-      </div>
+      {
+        notificationData.map(notification => notificationItems(notification))
+      }
     </div>
   )
 }
+
