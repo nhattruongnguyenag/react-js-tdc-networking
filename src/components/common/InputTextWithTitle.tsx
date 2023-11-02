656c7389b8aa34b7b16136fe95c30419bae33857
Nguyen Nhat Truong <nhattruongnguyenag@gmail.com>
2023-11-02 14:14:48 +0700

    complete create recruitment post function

--- src/components/common/InputTextWithTitle.tsx
+++ src/components/common/InputTextWithTitle.tsx
@@ -5,17 +5,26 @@ interface InputTextWithTitleProps {
     placeholder?: string
     type?: string
     name?: string
+    onTextChange?: (value: string) => void
+    defaultValue?: string
 }
 
-export default function InputTextWithTitle(props: InputTextWithTitleProps) {
+const InputTextWithTitle = React.forwardRef((props: InputTextWithTitleProps, ref: React.LegacyRef<HTMLInputElement>) => {
     return (
-        <div className='col-lg-12 mb-3'>
+        <div className='col-lg-12 mt-3'>
             <div className='form-gorup'>
-                <label className='mont-font fw-600 font-xssss text-dark'>{props.title ?? 'Title'}</label>
+                <label className='mont-font fw-600 font-xssss text-dark mb-2'>{props.title ?? 'Title'}</label>
                 <input
-                    {...props}
+                    ref={ref}
+                    defaultValue={props.defaultValue}
+                    onChange={(event) => props.onTextChange && props.onTextChange(event.target.value)}
+                    placeholder={props.placeholder}
+                    type={props.type ? props.type : 'text'}
+                    name={props.name}
                     className='form-control' />
             </div>
         </div>
     )
-}
+})
+
+export default InputTextWithTitle
--- src/components/common/TextAreaWithTitle.tsx
+++ src/components/common/TextAreaWithTitle.tsx
@@ -5,17 +5,22 @@ interface TextAreaWithTitleProps {
   rows?: number
   placeholder?: string
   value?: string
+  onTextChange?: (value: string) => void
 }
 
-export default function TextAreaWithTitle(props: TextAreaWithTitleProps) {
+const TextAreaWithTitle = React.forwardRef((props: TextAreaWithTitleProps, ref: React.LegacyRef<HTMLTextAreaElement>) => {
   return (
-    <div className='col-lg-12 mb-3'>
+    <div className='col-lg-12 mt-3'>
       <label className='mont-font fw-600 font-xsss text-dark mb-2'>{props.title ?? 'Title'}</label>
       <textarea
-        {...props}
+        ref={ref}
         className='form-control h-auto lh-16 mb-0 p-3'
         rows={props.rows ?? 5}
+        placeholder={props.placeholder}
+        onChange={(event) => props.onTextChange && props.onTextChange(event.target.value)}
       />
     </div>
   )
-}
+})
+
+export default TextAreaWithTitle
